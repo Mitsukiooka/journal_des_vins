a660e0b52f799dfe99b73726d9b4eeec96b74c8a
Mitsukiooka <66833185+Mitsukiooka@users.noreply.github.com>
2020-11-18 18:08:25 +0900

    edit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,10 +3,10 @@
 //= require bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
-//= require js/jquery.min
-//= require js/bootstrap
-//= require js/slick
-//= require js/simple-animated-counter
-//= require js/jquery.magnific-popup.min
-//= require js/bootstrap-datepicker
-//= require js/app
+//= require javascripts/jquery.min
+//= require javascripts/bootstrap
+//= require javascripts/slick
+//= require javascripts/simple-animated-counter
+//= require javascripts/jquery.magnific-popup.min
+//= require javascripts/bootstrap-datepicker
+//= require javascripts/app
